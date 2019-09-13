9bc15e381cc4c83a571de07e9074064b42dd98e2
Findlay Hannam <f.hannam@elsevier.com>
2019-09-13 17:25:29 +0100

    TDD - test still passing

---
+++ lessons/3 - TDD/index.test.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+const identifyVehicle = require('./');
+
+describe(__filename, () => {
+  it('Can identify a car', () => {
+    const car = {
+      wheels: 4,
+      treads: false,
+    };
+  });
+});
