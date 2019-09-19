8d0ee57779cd00fbff7bd4fd5d2357e2ad4ae45d
Findlay Hannam <f.hannam@elsevier.com>
2019-09-19 12:33:23 +0100

    Identify code branches

--- lessons/2-UnitTests/index.test.js
+++ lessons/2-UnitTests/index.test.js
@@ -3,11 +3,31 @@ const { isAllNumbers, isVowel } = require('./');
 
 describe(__filename, () => {
   describe('isAllNumbers', () => {
-    it('passes a simple test', () => {
+    it('An array of all numbers', () => {
       assert.equal(true, isAllNumbers([1]));
-    });  
+    });
+    it('Not an array', () => {
+
+    });
+    it('An empty array', () => {
+
+    });
+    it('An array containing something other than a number', () => {
+
+    });
   });
   describe('isVowel', () => {
+    it('A vowel', () => {
 
+    });
+    it('A consonant', () => {
+
+    });
+    it('A number', () => {
+
+    });
+    it('A string with length >1', () => {
+
+    });
   });
 });
